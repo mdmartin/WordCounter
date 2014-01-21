eac9b016445d2fcc981f67504b70ff4cb777a931
mdmartin <cn.md.martin@gmail.com>
2014-01-21 18:42:59 -0500

    Update argu_parser.java

--- argu_parser.java
+++ argu_parser.java
@@ -5,20 +5,26 @@ class argu_parser {
 	private String myDelimiters = ".?!";
 	private int myLimit = 3;
 	
+	
+	/* Function returns if the arguments are successfully parsed
+	 * @params the arguments
+	 * @return if arguments are correct, return true.
+	 */
 	public boolean feedArguments(String[] args) {
+
 		int argLen = args.length;
 		
-		if (argLen > 5) // too many arguments
+		if (argLen > 5) // Too many arguments. There will be at most 5 arguments
 		{
 			System.err.println("Too many arguments.");
 			return false;
 		}
 
-		for (int i = 0; i < argLen;) // parse arguments. it actually only iterates the elements that have odd indices.
+		for (int i = 0; i < argLen;) // Parse arguments. It actually only iterates the elements that have odd indices.
 		{
-			if (!args[i].startsWith("-")) // not -l or -d. it could only be the file name
+			if (!args[i].startsWith("-")) // If it is not -l or -d, then it could only be a file's name.
 			{
-				if (args[i].endsWith(".txt") && i == (argLen - 1)) {
+				if (args[i].endsWith(".txt") && i == (argLen - 1)) { // Only .txt file is acceptable
 					setHasFile(true);
 					setMyFileName(args[argLen - 1]);
 					break;
@@ -27,13 +33,13 @@ class argu_parser {
 					return false;
 				}
 			}
-			else if (!(args[i].equals("-d") || args[i].equals("-l"))) // not -d, -l, or file name
+			else if (!(args[i].equals("-d") || args[i].equals("-l"))) // If it is not -d, -l, or file name, then it's an illegal argument.
 			{
 				System.err.println(args[i] + " Improper input argument. Please check user manual.");
 				return false;
 			}
 			
-			else if (args[i].equals("-d")) // -d. then the next argument is the string of customized delimiters
+			else if (args[i].equals("-d")) // -d. then the next argument should be a string of customized delimiters
 			{
 				if (i < argLen - 1) {
 						setMyDelimiters(new String(args[i + 1]));
@@ -42,12 +48,11 @@ class argu_parser {
 					return false;
 				}
 				i += 2;
-				//continue;
 			}
-			else if (args[i].equals("-l")) // -l. then the next argument has to be an integer and is the customized limitation of word length
+			else if (args[i].equals("-l")) // -l. then the next argument should be an integer and be the customized limitation of word length
 			{
 				if (i < argLen - 1) {
-					if (isInteger(args[i + 1])) {
+					if (isInteger(args[i + 1])) { // judge if the next argument is an integer
 						setMyLimit(Integer.valueOf(args[i + 1]));
 					} else {
 						System.err.println("Invalid Integer argument as the word length limitation. Please re-run the program with a proper integer.");
@@ -63,6 +68,10 @@ class argu_parser {
 		return true;
 	}
 
+	/* Function returns if a string could be parsed as an integer
+	 * @params a string
+	 * @return if the string could be parsed as an integer, return true.
+	 */
 	private boolean isInteger(String s) {
 		try {
 			Integer.parseInt(s);
@@ -71,6 +80,8 @@ class argu_parser {
 		}
 		return true;
 	}
+	
+	// setters and getters
 	
 	public boolean isHasFile() {
 		return hasFile;
