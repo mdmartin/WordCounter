6d3db9f2069b63820e9a3d15cb5d13c367eba3ae
mdmartin <cn.md.martin@gmail.com>
2014-01-20 22:37:54 -0500

    Update argu_parser.java

--- argu_parser.java
+++ argu_parser.java
@@ -1,151 +1,110 @@
-
 class argu_parser {
-	private static boolean hasDelimiter = false;
-	private static boolean hasFile = false;
-	private static String myFileName = null;
-	private static String myDelimiters = null;
-	private static int myLimit = 3;
+	private boolean hasFile = false;
+	private String myFileName = null;
+	private String myDelimiters = ".?!";
+	private int myLimit = 3;
 
-	public static void feedArguments(String[] args)
-	{
+	public boolean feedArguments(String[] args) {
 		int argLen = args.length;
 
 		if (argLen > 5) // too many arguments
 		{
 			System.err.println("Too many arguments.");
-			return;
+			return false;
 		}
 
-		for (int i = 0; i < argLen; ) // parse arguments. it actually only iterates the elements that have odd indices.
+		for (int i = 0; i < argLen;) // parse arguments. it actually only iterates the elements that have odd indices.
 		{
+			System.out.println(i);
+			System.out.println(argLen);
+			System.out.println(args[i].endsWith(".txt"));
+		
 			if (!args[i].startsWith("-")) // not -l or -d. it could only be the file name
 			{
-				if (args[i].endsWith(".txt") && i == argLen - 1)
-				{
+				if (args[i].endsWith(".txt") && i == (argLen - 1)) {
 					setHasFile(true);
 					setMyFileName(args[argLen - 1]);
-				}
-				else 
-				{
+					break;
+				} else {
 					System.err.println("Illegal arguments.");
-					return;
+					return false;
 				}
+				
 			}
 			if (!(args[i].equals("-d") || args[i].equals("-l"))) // not -d, -l, or file name
 			{
 				System.err.println("Illegal arguments.");
-				return;
+				return false;
 			}
 			if (args[i].equals("-d")) // -d. then the next argument is the string of customized delimiters
 			{
-				setHasDelimiter(true);
-				if (i < argLen - 1)
-				{
+				if (i < argLen - 1) {
 					setMyDelimiters(new String(args[i + 1]));
-				}
-				else 
-				{
+				} else {
 					System.err.println("Illegal arguments.");
-					return;
+					return false;
 				}
 				i += 2;
 				continue;
 			}
 			if (args[i].equals("-l")) // -l. then the next argument has to be an integer and is the customized limitation of word length
 			{
-				if (i < argLen - 1)
-				{
-					if (isInteger(args[i + 1]))
-					{
+				if (i < argLen - 1) {
+					if (isInteger(args[i + 1])) {
 						setMyLimit(Integer.valueOf(args[i + 1]));
-					}
-					else
-					{
+					} else {
 						System.err.println("Illegal arguments.");
-						return;
+						return false;
 					}
-				}
-				else 
-				{
+				} else {
 					System.err.println("Illegal arguments.");
-					return;
+					return false;
 				}
 				i += 2;
 			}
 		}
+
+		return true;
+	}
+
+	private boolean isInteger(String s) {
+		try {
+			Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
 	}
 	
-	
-	
-	private static boolean isInteger(String s) {
-	    try 
-	    { 
-	        Integer.parseInt(s); 
-	    } 
-	    catch(NumberFormatException e) 
-	    { 
-	        return false; 
-	    }
-	    return true;
-	}
-
-
-
-	public static boolean isHasDelimiter() {
-		return hasDelimiter;
-	}
-
-
-
-	public static void setHasDelimiter(boolean hasDelimiter) {
-		argu_parser.hasDelimiter = hasDelimiter;
-	}
-
-
-
-	public static boolean isHasFile() {
+	public boolean isHasFile() {
 		return hasFile;
 	}
 
-
-
-	public static void setHasFile(boolean hasFile) {
-		argu_parser.hasFile = hasFile;
+	public void setHasFile(boolean hasFile) {
+		this.hasFile = hasFile;
 	}
 
-
-
-	public static String getMyFileName() {
+	public String getMyFileName() {
 		return myFileName;
 	}
 
-
-
-	public static void setMyFileName(String myFileName) {
-		argu_parser.myFileName = myFileName;
+	public void setMyFileName(String myFileName) {
+		this.myFileName = myFileName;
 	}
 
-
-
-	public static String getMyDelimiters() {
+	public String getMyDelimiters() {
 		return myDelimiters;
 	}
 
-
-
-	public static void setMyDelimiters(String myDelimiters) {
-		argu_parser.myDelimiters = myDelimiters;
+	public void setMyDelimiters(String myDelimiters) {
+		this.myDelimiters = myDelimiters;
 	}
 
-
-
-	public static int getMyLimit() {
+	public int getMyLimit() {
 		return myLimit;
 	}
 
-
-
-	public static void setMyLimit(int myLimit) {
-		argu_parser.myLimit = myLimit;
+	public void setMyLimit(int myLimit) {
+		this.myLimit = myLimit;
 	}
 }
