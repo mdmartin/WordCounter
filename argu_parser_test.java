e036fadd41a2d80aa7d3dabdc1910ab6212267ae
Paul <bernier.pja@gmail.com>
2014-01-21 18:24:01 -0500

    more tests

--- argu_parser_test.java
+++ argu_parser_test.java
@@ -5,6 +5,34 @@ import org.junit.Test;
 public class argu_parser_test {
 
 	@Test
+	public void testFeedArgumentsGoodOption() {
+		argu_parser parser = new argu_parser();
+		String[] args = {"test.txt"};
+		assertTrue(parser.feedArguments(args));
+	}
+	
+	@Test
+	public void testFeedArgumentsGoodOption2() {
+		argu_parser parser = new argu_parser();
+		String[] args = {"-d", ".!;?", "test.txt"};
+		assertTrue(parser.feedArguments(args));
+	}
+	
+	@Test
+	public void testFeedArgumentsGoodOption3() {
+		argu_parser parser = new argu_parser();
+		String[] args = {"-l", "1", "test.txt"};
+		assertTrue(parser.feedArguments(args));
+	}
+	
+	@Test
+	public void testFeedArgumentsGoodOption4() {
+		argu_parser parser = new argu_parser();
+		String[] args = {"-d", ".!;?", "-l", "2", "test.txt"};
+		assertTrue(parser.feedArguments(args));
+	}
+	
+	@Test
 	public void testFeedArgumentsDefaultDelimiters() {
 		argu_parser parser = new argu_parser();
 		String[] args = {};
