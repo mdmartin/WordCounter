a8b50342ccd60688b0a799f3b9df99cfd8b8d790
Paul <bernier.pja@gmail.com>
2014-01-21 19:06:22 -0500

    clean unit test for parser

--- argu_parser_test.java
+++ argu_parser_test.java
@@ -10,14 +10,16 @@ public class argu_parser_test {
 		String[] args = {"test.txt"};
 		assertTrue(parser.feedArguments(args));
 		assertEquals(3, parser.getMyLimit());
+		assertEquals(".?!", parser.getMyDelimiters());
 	}
 	
 	@Test
 	public void testFeedArgumentsGoodOption2() {
 		argu_parser parser = new argu_parser();
-		String[] args = {"-d", ".!;?", "test.txt"};
+		String[] args = {"-d", ".?", "test.txt"};
 		assertTrue(parser.feedArguments(args));
 		assertEquals(3, parser.getMyLimit());
+		assertEquals(".?", parser.getMyDelimiters());
 	}
 	
 	@Test
@@ -26,22 +28,33 @@ public class argu_parser_test {
 		String[] args = {"-l", "1", "test.txt"};
 		assertTrue(parser.feedArguments(args));
 		assertEquals(1, parser.getMyLimit());
+		assertEquals(".?!", parser.getMyDelimiters());
 	}
 	
 	@Test
 	public void testFeedArgumentsGoodOption4() {
 		argu_parser parser = new argu_parser();
-		String[] args = {"-d", ".!;?", "-l", "2", "test.txt"};
+		String[] args = {"-d", ".!", "-l", "2", "test.txt"};
 		assertTrue(parser.feedArguments(args));
 		assertEquals(2, parser.getMyLimit());
+		assertEquals(".!", parser.getMyDelimiters());
 	}
 	
 	@Test
 	public void testFeedArgumentsGoodOption5() {
 		argu_parser parser = new argu_parser();
-		String[] args = {"-l", "6", "-d", ".!;?", "test.txt"};
+		String[] args = {"-l", "6", "-d", ".!", "test.txt"};
 		assertTrue(parser.feedArguments(args));
 		assertEquals(6, parser.getMyLimit());
+		assertEquals(".!", parser.getMyDelimiters());
+	}
+	
+	@Test
+	public void testFeedArgumentsGoodOption6() {
+		argu_parser parser = new argu_parser();
+		String[] args = {};
+		assertTrue(parser.feedArguments(args));
+
 	}
 	
 	@Test
@@ -53,55 +66,11 @@ public class argu_parser_test {
 	}
 	
 	@Test
-	public void testFeedArgumentsBadOptions() {
-		argu_parser parser = new argu_parser();
-		String[] args = {"-d", "-l", "5"};
-		assertFalse(parser.feedArguments(args));
-	}
-	
-	@Test
-	public void testFeedArgumentsBadOptions2() {
-		argu_parser parser = new argu_parser();
-		String[] args = {"-l", "-d", ".?!"};
-		assertFalse(parser.feedArguments(args));
-	}
-	
-	@Test
-	public void testFeedArgumentsBadOptions3() {
-		argu_parser parser = new argu_parser();
-		String[] args = {"-l", "not integer"};
-		assertFalse(parser.feedArguments(args));
-	}
-	
-	@Test
-	public void testFeedArgumentsEmptySetOfDelimited() {
-		argu_parser parser = new argu_parser();
-		String[] args = {"-d", ""};
-		assertTrue(parser.feedArguments(args));
-	}
-	
-	@Test
 	public void testFeedArgumentsDefaultWordLength() {
 		argu_parser parser = new argu_parser();
 		String[] args = {};
 		assertTrue(parser.feedArguments(args));
 		assertEquals(3, parser.getMyLimit());
-	}
-	
-	@Test
-	public void testFeedArgumentsWithSemicolon() {
-		argu_parser parser = new argu_parser();
-		String[] args = {"-d", ";.!?"};
-		assertTrue(parser.feedArguments(args));
-		assertEquals(";.!?", parser.getMyDelimiters());
-	}
-	
-	@Test
-	public void testFeedArgumentsWordLength() {
-		argu_parser parser = new argu_parser();
-		String[] args = {"-d", ";.!?"};
-		assertTrue(parser.feedArguments(args));
-		assertEquals(";.!?", parser.getMyDelimiters());
 	}
 	
 	@Test
@@ -117,5 +86,28 @@ public class argu_parser_test {
 		String[] args = {"-l"};
 		assertFalse(parser.feedArguments(args));
 	}
+	
+	@Test
+	public void testFeedArgumentsBadOptions3() {
+		argu_parser parser = new argu_parser();
+		String[] args = {"-l", "not integer"};
+		assertFalse(parser.feedArguments(args));
+	}
+	
+	@Test
+	public void testFeedArgumentsWithSemicolon() {
+		argu_parser parser = new argu_parser();
+		String[] args = {"-d", ";.?!"};
+		assertTrue(parser.feedArguments(args));
+		assertEquals(";.?!", parser.getMyDelimiters());
+	}
+	
+	@Test
+	public void testFeedArgumentsBadOptions() {
+		argu_parser parser = new argu_parser();
+		String[] args = {"-z"};
+		assertFalse(parser.feedArguments(args));
+	}
+
 
 }
